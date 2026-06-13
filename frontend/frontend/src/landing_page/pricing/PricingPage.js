0dc1cefd956716eaa9c2daeea11cfaa184dac10e
Dolly Kushwaha <dollykush1804@gmail.com>
2026-06-13 09:38:51 +0530

    Setting up the pricing section

--- frontend/frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/frontend/src/landing_page/pricing/PricingPage.js
@@ -1,21 +1,17 @@
-import React from 'react';
-
-import Hero from '../home/Hero';
-import Brokerage from './Brokerage';
-
-import OpenAccount from '../../OpenAccount';
-
+import React from "react";
+import Hero from "./Hero";
+import Brokerage from "./Brokerage";
+import OpenAccount from "../../OpenAccount";
 
 function PricingPage() {
-    return (
-        <>
-       
-        <Hero/>
-        <Brokerage/>
-        <OpenAccount/>
-       
-        </>
-      );
+  return (
+    <>
+      <Hero />
+      <OpenAccount />
+      
+      <Brokerage />
+    </>
+  );
 }
 
 export default PricingPage;
