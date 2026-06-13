f8ed1b4c8cacc3abcd1faa8a4e5173c96cc0e9d1
Dolly Kushwaha <dollykush1804@gmail.com>
2026-06-13 09:40:02 +0530

    Removing the image from OpenAccount

--- frontend/frontend/src/OpenAccount.js
+++ frontend/frontend/src/OpenAccount.js
@@ -5,7 +5,7 @@
     
                 <div className = 'container p-5 mb-5'>
                       <div className = 'row text-center'>
-                         <img src="/media/images/homeHero.png" alt="Hero" className= 'mb-5'/>
+                        
                          <h1 className= 'mt-5'>Open a Zerodha account</h1>
                          <p>
                             Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.
