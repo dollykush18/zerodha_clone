3a7dc63c63b308568a4ceb93795c7971a7fbd84d
Dolly Kushwaha <dollykush1804@gmail.com>
2026-06-18 13:08:33 +0530

    Updating the Components

--- dashboard/src/components/DoughnoutChart.js
+++ dashboard/src/components/DoughnoutChart.js
@@ -5,5 +5,5 @@ import { Doughnut } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export function DoughnutChart({ data }) {
-  return <Doughnut data={data} />;
+  return <Doughnut data={data} />
 }
