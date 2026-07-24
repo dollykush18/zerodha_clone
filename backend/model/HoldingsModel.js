7234f36fbf4360d232e890e599cd47a58076a02e
Dolly Kushwaha <dollykush1804@gmail.com>
2026-07-24 19:10:53 +0530

    Setting up the Backend

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -3,5 +3,5 @@ const {HoldingsSchema} = require('../schemas/HoldingsSchema');
 
 const HoldingsModel = new model("holding", HoldingsSchema);
 
-module.exports = {HoldingsModel}
+module.exports = {HoldingsModel};
  
