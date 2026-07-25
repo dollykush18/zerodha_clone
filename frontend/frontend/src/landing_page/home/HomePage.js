1f156e601bce3946bc1885a957282a712a14efe3
Dolly Kushwaha <dollykush1804@gmail.com>
2026-07-25 20:46:15 +0530

    Remove duplicate navbar from homepage

--- frontend/frontend/src/landing_page/home/HomePage.js
+++ frontend/frontend/src/landing_page/home/HomePage.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import Hero from './Hero';
-import Awards from './Awards';
-import Pricing from './Pricing';
-import Stats from './Stats';
-import Education from './Education';
+import React from "react";
+import Hero from "./Hero";
+import Awards from "./Awards";
+import Pricing from "./Pricing";
+import Stats from "./Stats";
+import Education from "./Education";
+import OpenAccount from "../../OpenAccount";
+import Footer from "../../Footer";
 
-import Navbar from '../../Navbar';
-import OpenAccount from '../../OpenAccount';
-import Footer from '../../Footer';
 function HomePage() {
-    return ( 
-        <>
-             <Navbar />
-             <Hero />
-             <Awards />
-             <Stats />
-             <Pricing />
-             <Education />
-             <OpenAccount />
-             <Footer />
-
-         </>    
-     );
+  return (
+    <>
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <OpenAccount />
+      <Footer />
+    </>
+  );
 }
 
 export default HomePage;
